d572bac9cb88aa395ca5fd80755755bdcadd5e04
ayoubkasimi58-cpu <ayoubkasimi58@gmail.com>
2025-10-28 15:30:13 +0100

    Add files via upload

---
+++ 2 - Chapitre  Appels AJAX avec fetch() & traitement JSON/script.js
@@ -0,0 +1,10 @@
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then(Response => Response.json())
+.then(data => {
+    console.log("fgfgfggf:",data);
+})
+.catch(error => {
+    console.error("hdhhdfhf:",error)
+})
+
+
